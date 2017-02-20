6f020b9381d04fe693ae8b9d6a880b57f5021000
Vadim Laletin <rusmiligamer@gmail.com>
2017-02-20 20:46:12 +0700

    Cover missing branch with direct FK

--- tests/test_writable_nested_model_serializer.py
+++ tests/test_writable_nested_model_serializer.py
@@ -189,6 +189,70 @@ class WritableNestedModelSerializerTest(TestCase):
         self.assertEqual(Avatar.objects.count(), 2)
         self.assertEqual(AccessKey.objects.count(), 1)
 
+    def test_partial_update_direct_fk(self):
+        serializer = UserSerializer(data=self.get_initial_data())
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        # Check instances count
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(Profile.objects.count(), 1)
+        self.assertEqual(Site.objects.count(), 2)
+        self.assertEqual(Avatar.objects.count(), 2)
+        self.assertEqual(AccessKey.objects.count(), 1)
+
+        # Partial update
+        user_pk = user.pk
+        profile_pk = user.profile.pk
+        access_key_pk = user.profile.access_key.pk
+
+        serializer = UserSerializer(
+            instance=user,
+            partial=True,
+            data={
+                'pk': user_pk,
+                'profile': {
+                    'pk': profile_pk,
+                    'access_key': {
+                        'pk': access_key_pk,
+                        'key': 'new',
+                    }
+                },
+            }
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        user.refresh_from_db()
+        self.assertIsNotNone(user)
+        self.assertEqual(user.pk, user_pk)
+        self.assertEqual(user.username, 'test')
+
+        profile = user.profile
+        self.assertIsNotNone(profile)
+        access_key = profile.access_key
+        self.assertIsNotNone(access_key)
+        self.assertEqual(access_key.key, 'new')
+        self.assertEqual(access_key.pk, access_key_pk)
+
+        self.assertEqual(profile.pk, profile_pk)
+        self.assertEqual(profile.sites.count(), 2)
+        self.assertSetEqual(
+            set(profile.sites.values_list('url', flat=True)),
+            {'http://google.com', 'http://yahoo.com'}
+        )
+        self.assertEqual(profile.avatars.count(), 2)
+        self.assertSetEqual(
+            set(profile.avatars.values_list('image', flat=True)),
+            {'image-1.png', 'image-2.png'}
+        )
+
+        # Check instances count
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(Profile.objects.count(), 1)
+        self.assertEqual(Site.objects.count(), 2)
+        self.assertEqual(Avatar.objects.count(), 2)
+        self.assertEqual(AccessKey.objects.count(), 1)
+
     def test_nested_partial_update(self):
         serializer = UserSerializer(data=self.get_initial_data())
         serializer.is_valid(raise_exception=True)
