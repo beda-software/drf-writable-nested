177d64955e2ee51fe08c4687296e07b6022e0060
ruscoder <rusmiligamer@gmail.com>
2018-07-04 14:02:47 +0300

    PEP8

--- tests/utils.py
+++ tests/utils.py
@@ -4,7 +4,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 def get_sample_file(name, content=b'*'):
-        with tempfile.NamedTemporaryFile() as tf:
-            tf.file.write(content)
-            tf.file.seek(0)
-            return SimpleUploadedFile(name, tf.file.read())
+    with tempfile.NamedTemporaryFile() as tf:
+        tf.file.write(content)
+        tf.file.seek(0)
+        return SimpleUploadedFile(name, tf.file.read())
