d45b684054c93613aa55d600ee62e438508f87c9
johnthagen <johnthagen@gmail.com>
2020-04-05 10:57:34 -0400

    Remove Django 2.1 trove classifier

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
         'Intended Audience :: Developers',
