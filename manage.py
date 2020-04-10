285d09067dce830eb57c42766f0742ae5d7eabcd
johnthagen <johnthagen@gmail.com>
2020-04-09 20:31:38 -0400

    Make test Model migrations

---
+++ manage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""Django's command-line utility for administrative tasks.
+
+This script is needed to recreate test Model migrations. To do that:
+
+$ python manage.py makemigrations tests
+"""
+
+import os
+import sys
+
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
