d972338defa4b49516a4d72d344f0e3513e4cd5b
Vadim Laletin <ruscoder@users.noreply.github.com>
2020-04-12 11:07:59 +0700

    Bump to 0.6.0

--- drf_writable_nested/__init__.py
+++ drf_writable_nested/__init__.py
@@ -1,8 +1,8 @@
 __title__ = 'DRF writable nested'
-__version__ = '0.5.4'
+__version__ = '0.6.0'
 __author__ = 'beda.software'
 __license__ = 'BSD 2-Clause'
-__copyright__ = 'Copyright 2014-2019 beda.software'
+__copyright__ = 'Copyright 2014-2020 beda.software'
 
 # Version synonym
 VERSION = __version__
