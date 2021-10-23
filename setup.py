57937909717dec37a0d1a8e53d0fda38b8be7298
johnthagen <johnthagen@users.noreply.github.com>
2021-10-23 11:50:17 -0400

    Add Python 3.10 to test matrix

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
