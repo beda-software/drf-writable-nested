a36220f8038142a4500da13b7dff338a6998f838
Ilya Beda <ir4y.ix@gmail.com>
2022-04-03 13:43:41 +0200

    Drop support python 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     },
     include_package_data=True,
     zip_safe=False,
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -55,8 +55,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
