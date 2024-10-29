97378b231e4205b31dea4dd5c4655f00e0765e95
Bruno Alla <alla.brunoo@gmail.com>
2024-10-29 08:59:16 +0000

    Add official support for Django 5.1 and Python 3.13

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 4.2',
         'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
@@ -56,6 +57,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
